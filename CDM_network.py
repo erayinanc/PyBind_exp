039ea2c5141ae046a2a343310df05c2d3fde450d
Eray Inanc <erayinanc1@gmail.com>
2023-10-19 11:15:56 +0200

    Update CDM_network.py

--- CDM_network.py
+++ CDM_network.py
@@ -1,25 +1,20 @@
 """
-derived from U-net:
-https://github.com/milesial/Pytorch-UNet/blob/master/unet/
-mini version for dev
+derived from https://github.com/milesial/Pytorch-UNet/blob/master/unet/
 """
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-# mini U-NET with only single bottom layer (for dev)
 class mini_U_Net(nn.Module):
-    def __init__(self,img_ch=1,output_ch=1,bilinear=True):
+    def __init__(self,img_ch=1,output_ch=1):
         super().__init__()
-        fac = 2 if bilinear else 1
-
         self.Maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
 
         self.Conv1 = conv_block(ch_in=img_ch, ch_out=64)
-        self.Conv2 = conv_block(ch_in=64, ch_out=128 // fac)
+        self.Conv2 = conv_block(ch_in=64, ch_out=128)
 
-        self.Up2 = up_conv(ch_in=128, bilinear=bilinear)
-        self.Up_conv2 = conv_block(ch_in=128, ch_out=64, bilinear=bilinear)
+        self.Up2 = up_conv(ch_in=128)
+        self.Up_conv2 = conv_block(ch_in=128, ch_out=64)
 
         self.Conv_1x1 = nn.Conv2d(64, output_ch, kernel_size=1, stride=1, padding=0)
 
@@ -38,15 +33,13 @@ class mini_U_Net(nn.Module):
         return self.Conv_1x1(d2)
 
 class conv_block(nn.Module):
-    """ added bilinear threatment """
-    def __init__(self,ch_in,ch_out,bilinear=False):
+    def __init__(self,ch_in,ch_out):
         super().__init__()
-        ch_mid = ch_out if not bilinear else ch_in // 2
         self.double_conv = nn.Sequential(
-            nn.Conv2d(ch_in, ch_mid, kernel_size=3, stride=1, padding=1, bias=True),
-            nn.BatchNorm2d(ch_mid),
+            nn.Conv2d(ch_in, ch_out, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.BatchNorm2d(ch_out),
             nn.ReLU(inplace=True),
-            nn.Conv2d(ch_mid, ch_out, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.Conv2d(ch_out, ch_out, kernel_size=3, stride=1, padding=1, bias=True),
             nn.BatchNorm2d(ch_out),
             nn.ReLU(inplace=True)
             )
@@ -55,32 +48,19 @@ class conv_block(nn.Module):
         return self.double_conv(x)
 
 class up_conv(nn.Module):
-    """ added bilinear threatment """
-    def __init__(self,ch_in,bilinear=False):
+    def __init__(self,ch_in):
         super().__init__()
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        else:
-            self.up = nn.ConvTranspose2d(ch_in, ch_in // 2, kernel_size=2, stride=2)
+        self.up = nn.ConvTranspose2d(ch_in, ch_in // 2, kernel_size=2, stride=2)
 
     def forward(self,x):
         return self.up(x)
 
 def up_cat(x1,x2):
-    """
-    fixes dimension problems with concatanate 
-    if you have padding issues, see
-    https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
-    https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
-    """
     return torch.cat((x1,up_pad(x1,x2)),dim=1)
 
 def up_pad(x1,x2):
-    """ fixes dimension problems with padding 
-    pads smaller x2 to larger x1 so x1 and x2 have the same dimensions
-    """
     diffY = x1.size()[2] - x2.size()[2]
     diffX = x1.size()[3] - x2.size()[3]
     return F.pad(x2, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
 
-# EO
+# EOF
